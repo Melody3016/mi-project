6c0cba4b60e6c6e41b3e31e17934871d7717768e
xuanlvzeng <xuanlvzeng@gmail.com>
2020-08-25 09:51:32 +0800

    分类列表后台部分完成

---
+++ src/admin/js/category.js
@@ -0,0 +1,22 @@
+$(function(){
+    // 发送ajax请求获取数据，并渲染到页面上
+    $.ajax({
+        type: "get",
+        url: "../../interface/categoryList.php",
+        beforeSend:function(){
+            /*开启这个进度条.*/
+            NProgress.configure();
+            NProgress.start();
+        },
+        data: {},
+        dataType: "json",
+        success: function (response) {
+            // 使用模板字符串渲染
+            var htmlStr = template('list',{'data':response});
+            $('tbody').html(htmlStr);
+        },
+        complete: function () {
+            NProgress.done();
+        }
+    });
+})
